9bfff9b6e55e24eff1e97e2cdb3cda12f7288d21
Shahardc4 <74099020+Shahardc4@users.noreply.github.com>
2022-03-28 21:47:47 +0300

    Updates

---
+++ precache-manifest.72bd8989da31ac474e9ed1a3c7866fb3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cf5baeb3766156814edb53c4230e39",
+    "url": "/IR-engine/index.html"
+  },
+  {
+    "revision": "76a44f74fa9299678a41",
+    "url": "/IR-engine/static/css/main.a33e412f.chunk.css"
+  },
+  {
+    "revision": "630aa22eebfa1a497e70",
+    "url": "/IR-engine/static/js/2.265ec63d.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/IR-engine/static/js/2.265ec63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a44f74fa9299678a41",
+    "url": "/IR-engine/static/js/main.ede55936.chunk.js"
+  },
+  {
+    "revision": "91d4fa5a78157ce6590b",
+    "url": "/IR-engine/static/js/runtime-main.9d7de136.js"
+  },
+  {
+    "revision": "d09d8b278480ae3a731826d84f53ca16",
+    "url": "/IR-engine/static/media/IR Project Main.d09d8b27.png"
+  },
+  {
+    "revision": "15e38cfc3e6c0f071da976474b91ffcc",
+    "url": "/IR-engine/static/media/big indices runs.15e38cfc.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/IR-engine/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "ded86812e1501ff74ee65bad4ab546de",
+    "url": "/IR-engine/static/media/download to the environment.ded86812.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/IR-engine/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/IR-engine/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "7dc59d2293215b11def41078141d2049",
+    "url": "/IR-engine/static/media/inverted index representation.7dc59d22.jpg"
+  },
+  {
+    "revision": "dd5db277470ec9f1cf135786604793f0",
+    "url": "/IR-engine/static/media/returning and ranking.dd5db277.jpg"
+  },
+  {
+    "revision": "32b85c244e601b93713315abb0287fa3",
+    "url": "/IR-engine/static/media/small indices runs.32b85c24.jpg"
+  },
+  {
+    "revision": "d0b35981d25199a9c3b1b070f4ad135c",
+    "url": "/IR-engine/static/media/uploading.d0b35981.jpg"
+  }
+]);
